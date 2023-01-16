2fcff2641b61d031b2f418014f48fbe71f86644a
Alessandro Dias <alessandro.valerio.dias@gmail.com>
2023-01-15 23:11:41 -0300

    refactoring main function

--- 01-statement-emitter/src/billprint.js
+++ 01-statement-emitter/src/billprint.js
@@ -1,20 +1,23 @@
 function statement (invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
+    return renderPlainText(statementData, plays);
+}
 
-    function renderPlainText(invoice, plays) {
-        let result = `Statement for ${invoice.customer}\n`;
-        for (let perf of invoice.performances) {
-            // print line for this order
-            result += `  ${playFor(perf).name} : ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
-        }
-        result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
-        result += `You earned ${totalVolumeCredits()} credits\n`;
-        return result;
+function renderPlainText(data, plays) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
+        // print line for this order
+        result += `  ${playFor(perf).name} : ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
     }
+    result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
+    return result;
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
         return result;
@@ -22,7 +25,7 @@ function statement (invoice, plays) {
 
     function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
         }
         return result;
