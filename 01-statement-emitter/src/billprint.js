18affea0690543b5e3dfeb9b7a8d429859b58b1c
Alessandro Dias <alessandro.valerio.dias@gmail.com>
2023-02-03 21:33:41 -0300

    Refactoring renderPlainText

--- 01-statement-emitter/src/billprint.js
+++ 01-statement-emitter/src/billprint.js
@@ -1,54 +1,45 @@
 function statement (invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
-}
 
-function renderPlainText(data, plays) {
-    let result = `Statement for ${data.customer}\n`;
-    for (let perf of data.performances) {
-        // print line for this order
-        result += `  ${playFor(perf).name} : ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
-    }
-    result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
-    return result;
-
-    function totalAmount() {
+    function totalVolumeCredits(data) {
         let result = 0;
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
     }
 
-    function totalVolumeCredits() {
+    function totalAmount(data) {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
+            result += perf.amount;
         }
         return result;
     }
 
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber);
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        result.amount = amountFor(result)/100;
+        result.volumeCredits = volumeCreditsFor(result);
+        return result;
     }
 
     function volumeCreditsFor(aPerformance) {
         let result = Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === playFor(aPerformance).type)
+        if ("comedy" === aPerformance.play.type)
             result += Math.floor(aPerformance.audience / 5);
         return result;
     }
 
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
     function amountFor(aPerformance) {
         let result = 0;
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -63,8 +54,32 @@ function renderPlainText(data, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+                throw new Error(`unknown type: ${aPerformance.play.type}`);
         }
         return result;
     }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+}
+
+function renderPlainText(data, plays) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
+        // print line for this order
+        result += `  ${perf.play.name} : ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+    result += `Amount owed is ${usd(totalAmount())}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
+    return result;
+
+    
+
+  
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber);
+    }
+
 }
